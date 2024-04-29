2a53ade7e425688320ea15c0a4882297eb7270e1
BelkyDan <belchordaniel123@gmail.com>
2024-04-29 13:50:23 -0300

    findByDepartment

--- src/Models/dao/SellerDao.java
+++ src/Models/dao/SellerDao.java
@@ -2,14 +2,20 @@ package Models.dao;
 
 import java.util.List;
 
+import ModelsEntities.Department;
 import ModelsEntities.Seller;
 
 public interface SellerDao {
-	
-	
+
 	void insert(Seller obj);
+
 	void update(Seller obj);
+
 	void deleteById(Seller obj);
+
 	Seller findById(Integer id);
+
 	List<Seller> findAll();
+	
+	List<Seller> findByDepartment(Department department);
 }
