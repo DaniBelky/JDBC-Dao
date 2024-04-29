c0187645da6dfe150b091005605b190bc93acaf7
BelkyDan <belchordaniel123@gmail.com>
2024-04-29 12:18:27 -0300

    Departamento

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import ModelsEntities.Departiments;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Departiments obg = new Departiments(1, "books");
+		System.out.println(obg);
+	}
+}
